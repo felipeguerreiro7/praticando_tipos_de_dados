9793f64b55967b246771ac2e6567258a1e6a286e
felipeguerreiro7 <lf.guerreiroalves@gmail.com>
2022-01-06 06:14:07 -0300

    Exercicio feito

---
+++ Exercicio 1/index.js
@@ -0,0 +1,4 @@
+const palavra = "algo"
+console.log('Esse e com aspas simples')
+console.log("Esse e com aspas duplas")
+console.log(`esse e com literals`)
---
+++ Exercicio 2/index.js
@@ -0,0 +1,3 @@
+console.log(7)
+console.log(7.5)
+console.log(5 + 4)
